8d105c317e71bf3d2bed34539f5fb676fe91faff
luca <l.juventino@live.it>
2018-12-03 17:28:30 +0100

    Search finito

--- X_Project.py
+++ X_Project.py
@@ -2,9 +2,10 @@
 Questo è il main code del progetto richiesto per il midterm pratico di ingegneria degli algoritmi
 """
 
-
+from Dictionary import Dictionary
 from linkedListDictionary import LinkedListDictionary
 from avlTree import AVLTree
+from binarySearchTree import BinarySearchTree
 
 class Partizione:
     def __init__(self, min, max, b):
@@ -25,8 +26,8 @@ class Partizione:
             if (self.checkType(self.list[self.d+1])):
                 self.list[self.d+1].insert(key, value)
                 print("elemtento inserito")
-                if(self.list[self.d+1].theList.len_list() == 6):
-                    self.changeAVL(self.list[self.d+1])
+                if(self.list[self.d+1].theList.len_list() >= 6):
+                    self.changeAVL(self.list[self.d+1]) # non funziona
             else:
                 self.list[self.d + 1].insert(key, value)
                 print("elemtento inserito")
@@ -51,10 +52,27 @@ class Partizione:
                     else:
                         self.list[i].insert(key, value)
                         print("elemtento inserito")
+                        break
 
-    def serch(self, key):
-        pass
-
+    def serch(self, key):               #da abbellire
+        if (key >= self.max):
+            if (self.checkType(self.list[self.d+1])):
+                print(self.list[self.d+1].search(key))
+            else:
+                print(self.list[self.d+1].search(key))
+        elif (key < self.min):
+            if (self.checkType(self.list[self.d])):
+                print(self.list[self.d].search(key))
+            else:
+                print(self.list[self.d].search(key))
+        else:
+            for i in range(self.d):
+                if(key >= self.min + (i*self.b) and key < self.min + ((i + 1) * self.b)):
+                    if(self.checkType(self.list[i])):
+                        print(self.list[i].search(key))
+                        break
+                else:
+                    print(self.list[i].search(key))
 
     def delete(self, key):
         pass
@@ -77,11 +95,10 @@ class Partizione:
             current = current.next
 
 
+
+
 if __name__ == "__main__":
     dic = Partizione(1, 17, 8)
-
-    dic.insert1(3, "prova 1")
-    dic.insert1(1, "prova 2")
 
     dic.insert1(19, "prova 2")
     dic.insert1(20, "prova 2")
@@ -89,4 +106,14 @@ if __name__ == "__main__":
     dic.insert1(22, "prova 2")
     dic.insert1(23, "prova 2")
     dic.insert1(24, "prova 2")
+    dic.insert1(25, "prova 2")
+    dic.insert1(26, "prova 2")
 
+    dic.serch(26)
+
+
+    if type(dic.list[3]) is AVLTree:
+        print("funziona")
+
+    else:
+        print("non funziona")
