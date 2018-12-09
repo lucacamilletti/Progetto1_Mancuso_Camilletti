7d34f1448cd81992bbbe57b0b379a7c902e1c919
luca <l.juventino@live.it>
2018-12-09 16:53:56 +0100

    Ultimi aggiustamenti

--- Main.py
+++ Main.py
@@ -2,15 +2,16 @@ from Progetto_1 import Partizione
 from time import time
 import cProfile
 import pstats
-a = [10,20,40,100]
-f = [1000,5000,10000,30000,50000,80000,100000]
+a = [ 7, 10, 20, 40, 100]
+f = [ 1000, 5000, 10000, 30000, 50000, 80000, 100000]
+
 
 for k in f:
     for j in a:
         t = str(k)
         t2 = str(j)
         print("Risultati per "+ t+ " elementi con partizione grandi "+ t2 + " : ")
-        dic = Partizione(j, k + j, j)
+        dic = Partizione(j, int(k/j)*j + j, j)
 
         start = time()
         cProfile.run('for i in range(k): dic.insert_main(i, i*3)', 'fileOutput')
