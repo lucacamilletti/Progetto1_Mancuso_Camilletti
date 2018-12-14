b6e7005dd6148fc6b0fef351135159c6cb76cbad
luca <l.juventino@live.it>
2018-12-14 16:52:48 +0100

    Ultimi aggiustamenti

--- Main.py
+++ Main.py
@@ -5,10 +5,11 @@ import pstats
 a = [7, 10, 20, 40, 100]
 f = [1000, 5000, 10000, 30000, 50000, 80000, 100000]
 
-for seq in range(1,2):
-    for k in f:
-        '''for j in a:
-            t = str(k)
+for k in f:
+    t = str(k)
+    for seq in range(1, 3):
+        for j in a:
+        
             t2 = str(j)
             print("Risultati per " + t + " elementi con partizione grandi "+ t2 + " : ")
             dic = Partizione(j, int(k/j)*j + j, j)
@@ -35,32 +36,35 @@ for seq in range(1,2):
             p.strip_dirs().sort_stats("time").print_stats()
             temp_delete = time() - start
 
-            print(temp_delete)'''
+            print(temp_delete)
 
-        t = str(k)
-        dic = {}
+    dic = {}
 
-        print("Risultati per " + t + " elementi: ")
-        print("\nInserimento:")
-        start = time()
-        cProfile.run('for i in range(k): dic[i] = i*2', 'fileOutput')
-        p = pstats.Stats('fileOutput')
-        p.strip_dirs().sort_stats("time").print_stats()
-        temp_delete = time() - start
-        print(temp_delete)
+    print("Risultati per " + t + " elementi: ")
+    print("\nInserimento:")
+    start = time()
+    cProfile.run('for i in range(k): dic[i] = i*2', 'fileOutput')
+    p = pstats.Stats('fileOutput')
+    p.strip_dirs().sort_stats("time").print_stats()
+    temp_delete = time() - start
+    print(temp_delete)
 
-        print("\nSearch:")
-        start = time()
-        cProfile.run('for i in range(k): dic[i]', 'fileOutput')
-        p = pstats.Stats('fileOutput')
-        p.strip_dirs().sort_stats("time").print_stats()
-        temp_delete = time() - start
-        print(temp_delete)
+    print("\nSearch:")
+    start = time()
+    cProfile.run('for i in range(k): dic[i]', 'fileOutput')
+    p = pstats.Stats('fileOutput')
+    p.strip_dirs().sort_stats("time").print_stats()
+    temp_delete = time() - start
+    print(temp_delete)
 
-        print("\nDelete:")
-        start = time()
-        cProfile.run('for i in range(k): del dic[i]', 'fileOutput')
-        p = pstats.Stats('fileOutput')
-        p.strip_dirs().sort_stats("time").print_stats()
-        temp_delete = time() - start
-        print(temp_delete)
+    print("\nDelete:")
+    start = time()
+    cProfile.run('for i in range(k): del dic[i]', 'fileOutput')
+    p = pstats.Stats('fileOutput')
+    p.strip_dirs().sort_stats("time").print_stats()
+    temp_delete = time() - start
+    print(temp_delete)
+
+    cProfile.run('dic[1] = 2', 'fileOutput')
+    p = pstats.Stats('fileOutput')
+    p.strip_dirs().sort_stats("time").print_stats()
